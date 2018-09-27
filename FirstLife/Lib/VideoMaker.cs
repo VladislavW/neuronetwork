fa534c51a536503dc506bf4861e833cd346e3b60
Vladislav Kovalik <vladislav.kovalik@gmail.com>
2018-09-28 00:59:14 +0300

    start implementation

--- FirstLife/Lib/VideoMaker.cs
+++ FirstLife/Lib/VideoMaker.cs
@@ -1,17 +1,28 @@
 ﻿using System.Drawing;
+using Accord.IO;
 using Accord.Video.FFMPEG;
 
 namespace FirstLife.Lib
 {
-    public static class VideoMaker
+    public class VideoMaker
     {
         private static VideoFileWriter _writer;
         
         public static void NewWriter(string filename, int w, int h)
         {
-            _writer = new VideoFileWriter();
+            _writer = new VideoFileWriter
+            {
+                VideoCodec = VideoCodec.Default,
+                Width = w,
+                Height = h,
+                FrameRate = 30,
+                BitRate = 5500000
+            };
+            
+            //_writer.Save(filename);
             // create new AVI file and open it
-            _writer.Open(filename, w, h,30, VideoCodec.Default , 5500000);
+            _writer.Open(filename, "avi");// Save(filename);
+           // _writer.Flush();
             
         }
 
